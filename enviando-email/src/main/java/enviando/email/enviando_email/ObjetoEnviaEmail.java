f1ba6291e90ecde61a578e806e1e1ce64df6290f
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-12-16 15:13:44 -0300

    Lista de pdf no email

--- enviando-email/src/main/java/enviando/email/enviando_email/ObjetoEnviaEmail.java
+++ enviando-email/src/main/java/enviando/email/enviando_email/ObjetoEnviaEmail.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import javax.activation.DataHandler;
@@ -30,19 +32,14 @@ import com.itextpdf.text.xml.simpleparser.NewLineHandler;
 
 public class ObjetoEnviaEmail {
 
-	
 	private String UserName = "projetosjava2022@gmail.com";
 	private String PassWord = "Java2022";
-	
+
 	private String ListaDestinatario = "";
 	private String NomeRemetente = "";
 	private String AssuntoEmail = "";
 	private String CorpoDoEmail = "";
 
-	
-	
-	
-	
 	public ObjetoEnviaEmail(String ListaDestinatario, String NomeRemetente, String AssuntoEmail, String CorpoDoEmail) {
 		this.ListaDestinatario = ListaDestinatario;
 		this.NomeRemetente = NomeRemetente;
@@ -50,117 +47,188 @@ public class ObjetoEnviaEmail {
 		this.CorpoDoEmail = CorpoDoEmail;
 	}
 
-
-
-
-				/*METDODO PARA ENVIAR EMAIL NORMAL*/
+	/* METDODO PARA ENVIAR EMAIL NORMAL */
 
 	public void EnviarEmail(boolean EnvioHtmal) throws Exception {
-		
+
 		Properties properties = new Properties();
 		properties.put("mail.smtp.ssl.trust", "*");
-    	properties.put("mail.smtp.auth", "true");/*Autorização*/
-    	properties.put("mail.smtp.starttls", "true");/*Autenticação*/
-    	properties.put("mail.smtp.host", "smtp.gmail.com");/*Servidor Gmail Google*/
-    	properties.put("mail.smtp.port", "465");/*Porta Do Servidor*/
-    	properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a porta a ser utilizada pelo socket*/
-    	properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Class Socket De Conexão SMTP*/
-    	
-    	Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-    		@Override
-    		protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-    			// TODO Auto-generated method stub
-    			return new javax.mail.PasswordAuthentication(UserName, PassWord);
-    		}
-    	
-    	});
-    	
-    	Address [] toUser = InternetAddress.parse(ListaDestinatario);
-    	
-    	Message message = new MimeMessage(session);
-    	message.setFrom(new InternetAddress(UserName, NomeRemetente));/*QUEM ESTA ENVIANDO*/
-    	message.setRecipients(Message.RecipientType.TO, toUser);/*PARA QUEM ESTA ENVIANDO*/
-    	message.setSubject(AssuntoEmail);/*ASSUNTO DO EMAIL*/
-    	
-    	
-    	if (EnvioHtmal) {
-    		message.setContent(CorpoDoEmail, "text/html; charset=utf-8");
-    	}else {
-    		message.setText(CorpoDoEmail);/*CORPO DO EMAIL*/
-    	}
-    	
-    	Transport.send(message);/*ENVIANDO EMAIL*/
+		properties.put("mail.smtp.auth", "true");/* Autorização */
+		properties.put("mail.smtp.starttls", "true");/* Autenticação */
+		properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor Gmail Google */
+		properties.put("mail.smtp.port", "465");/* Porta Do Servidor */
+		properties.put("mail.smtp.socketFactory.port", "465");/* Especifica a porta a ser utilizada pelo socket */
+		properties.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");/* Class Socket De Conexão SMTP */
+
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+			@Override
+			protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+				// TODO Auto-generated method stub
+				return new javax.mail.PasswordAuthentication(UserName, PassWord);
+			}
+
+		});
+
+		Address[] toUser = InternetAddress.parse(ListaDestinatario);
+
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(UserName, NomeRemetente));/* QUEM ESTA ENVIANDO */
+		message.setRecipients(Message.RecipientType.TO, toUser);/* PARA QUEM ESTA ENVIANDO */
+		message.setSubject(AssuntoEmail);/* ASSUNTO DO EMAIL */
+
+		if (EnvioHtmal) {
+			message.setContent(CorpoDoEmail, "text/html; charset=utf-8");
+		} else {
+			message.setText(CorpoDoEmail);/* CORPO DO EMAIL */
+		}
+
+		Transport.send(message);/* ENVIANDO EMAIL */
 	}
-	
-	/*ESSE METODO SIMULA O PDF OU QUALQUER ARQUIVO QUE POSSA SER ENVIADO POR ANEXO NO EMAIL.*/
-	/*VOCÊ PODE PEGAR O ARQUIVO NO SEU BANCO DE DADOS BASE64, BYTE[], STREAM DE ARQUIVOS.*/
-	/*PODE ESTAR EM UM BANCO OU UMA PASTA*/
+
+	/*
+	 * ESSE METODO SIMULA O PDF OU QUALQUER ARQUIVO QUE POSSA SER ENVIADO POR ANEXO
+	 * NO EMAIL.
+	 */
+	/*
+	 * VOCÊ PODE PEGAR O ARQUIVO NO SEU BANCO DE DADOS BASE64, BYTE[], STREAM DE
+	 * ARQUIVOS.
+	 */
+	/* PODE ESTAR EM UM BANCO OU UMA PASTA */
 	private FileInputStream simuladordePdf() throws Exception {
 		Document document = new Document();
 		File file = new File("Anexo.pdf");
-		file.createNewFile();/*CRIANDO ARQUIVO EM TEMPO DE EXECUÇÃO*/
-		PdfWriter.getInstance(document, new FileOutputStream(file));/*ESCREVENDO O CONTEUDO DO FILE DENTRO DO DOCUMENTO PDF*/
-		
+		file.createNewFile();/* CRIANDO ARQUIVO EM TEMPO DE EXECUÇÃO */
+		PdfWriter.getInstance(document,
+				new FileOutputStream(file));/* ESCREVENDO O CONTEUDO DO FILE DENTRO DO DOCUMENTO PDF */
+
 		document.open();
 		document.add(new Paragraph("Testando envio de pdf"));
 		document.close();
-		
-		return new FileInputStream(file); /*RETORNANDO UM PDF EM BRANCO COM O TEXTO DO PARAGRAFO*/
-		
-	}
-	
-	
-		/*METODO PARA ENVIAR EMAIL COM ANEXO*/
-public void EnviarEmailAnexo(boolean EnvioHtmal) throws Exception {
-		
-		Properties properties = new Properties();
-		properties.put("mail.smtp.ssl.trust", "*");
-    	properties.put("mail.smtp.auth", "true");/*Autorização*/
-    	properties.put("mail.smtp.starttls", "true");/*Autenticação*/
-    	properties.put("mail.smtp.host", "smtp.gmail.com");/*Servidor Gmail Google*/
-    	properties.put("mail.smtp.port", "465");/*Porta Do Servidor*/
-    	properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a porta a ser utilizada pelo socket*/
-    	properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Class Socket De Conexão SMTP*/
-    	
-    	Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-    		@Override
-    		protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-    			// TODO Auto-generated method stub
-    			return new javax.mail.PasswordAuthentication(UserName, PassWord);
-    		}
-    	
-    	});
-    	
-    	Address [] toUser = InternetAddress.parse(ListaDestinatario);
-    	
-    	Message message = new MimeMessage(session);
-    	message.setFrom(new InternetAddress(UserName, NomeRemetente));/*QUEM ESTA ENVIANDO*/
-    	message.setRecipients(Message.RecipientType.TO, toUser);/*PARA QUEM ESTA ENVIANDO*/
-    	message.setSubject(AssuntoEmail);/*ASSUNTO DO EMAIL*/
-    	
-    	/*PARTE 1 DO EMAIL QUE É O TEXTO EA DESCRIÇÃO DO EAMIL*/
-    	
-    	MimeBodyPart CorpoEmail = new MimeBodyPart();
-    	
-    	
-    	if (EnvioHtmal) {
-    		CorpoEmail.setContent(CorpoDoEmail, "text/html; charset=utf-8");
-    	}else {
-    		CorpoEmail.setText(CorpoDoEmail);/*CORPO DO EMAIL*/
-    	}
-    	
-    	/*PARTE 2 DO EMAIL QUE OS ANEXOS*/
-    	MimeBodyPart AnexoDoEmail = new MimeBodyPart();
-    	AnexoDoEmail.setDataHandler(new DataHandler(new ByteArrayDataSource(simuladordePdf(), "application/pdf")));/*ADICIONANDO O CONTEUDO DO ANEXO E TIPO DO ARQUIVO*/
-    	AnexoDoEmail.setFileName("AnexoTest.pdf");
-    	
-    	Multipart multipart = new MimeMultipart();/*OBJETO QUE IRÁ JUNTAR AS PARTES DO EMAIL*/
-        multipart.addBodyPart(CorpoEmail);/*ADICIONANDO CORPO DO EMAIL*/
-        multipart.addBodyPart(AnexoDoEmail);/*ADICIONANDO ANEXO DO EMAIL*/
-        
-        message.setContent(multipart);
-    	
-    	Transport.send(message);/*ENVIANDO EMAIL*/
+
+		return new FileInputStream(file); /* RETORNANDO UM PDF EM BRANCO COM O TEXTO DO PARAGRAFO */
+
 	}
 
+	/* METODO PARA ENVIAR EMAIL COM ANEXO */
+	public void EnviarEmailAnexo(boolean EnvioHtmal) throws Exception {
+
+		Properties properties = new Properties();
+		properties.put("mail.smtp.ssl.trust", "*");
+		properties.put("mail.smtp.auth", "true");/* Autorização */
+		properties.put("mail.smtp.starttls", "true");/* Autenticação */
+		properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor Gmail Google */
+		properties.put("mail.smtp.port", "465");/* Porta Do Servidor */
+		properties.put("mail.smtp.socketFactory.port", "465");/* Especifica a porta a ser utilizada pelo socket */
+		properties.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");/* Class Socket De Conexão SMTP */
+
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+			@Override
+			protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+				// TODO Auto-generated method stub
+				return new javax.mail.PasswordAuthentication(UserName, PassWord);
+			}
+
+		});
+
+		Address[] toUser = InternetAddress.parse(ListaDestinatario);
+
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(UserName, NomeRemetente));/* QUEM ESTA ENVIANDO */
+		message.setRecipients(Message.RecipientType.TO, toUser);/* PARA QUEM ESTA ENVIANDO */
+		message.setSubject(AssuntoEmail);/* ASSUNTO DO EMAIL */
+
+		/* PARTE 1 DO EMAIL QUE É O TEXTO EA DESCRIÇÃO DO EAMIL */
+
+		MimeBodyPart CorpoEmail = new MimeBodyPart();
+
+		if (EnvioHtmal) {
+			CorpoEmail.setContent(CorpoDoEmail, "text/html; charset=utf-8");
+		} else {
+			CorpoEmail.setText(CorpoDoEmail);/* CORPO DO EMAIL */
+		}
+
+		/* PARTE 2 DO EMAIL QUE OS ANEXOS */
+		MimeBodyPart AnexoDoEmail = new MimeBodyPart();
+		AnexoDoEmail.setDataHandler(new DataHandler(new ByteArrayDataSource(simuladordePdf(),
+				"application/pdf")));/* ADICIONANDO O CONTEUDO DO ANEXO E TIPO DO ARQUIVO */
+		AnexoDoEmail.setFileName("AnexoTest.pdf");
+
+		Multipart multipart = new MimeMultipart();/* OBJETO QUE IRÁ JUNTAR AS PARTES DO EMAIL */
+		multipart.addBodyPart(CorpoEmail);/* ADICIONANDO CORPO DO EMAIL */
+		multipart.addBodyPart(AnexoDoEmail);/* ADICIONANDO ANEXO DO EMAIL */
+
+		message.setContent(multipart);
+
+		Transport.send(message);/* ENVIANDO EMAIL */
+	}
+
+	public void EnviarEmailListaDeAnexo(boolean EnvioHtmal) throws Exception {
+
+		Properties properties = new Properties();
+		properties.put("mail.smtp.ssl.trust", "*");
+		properties.put("mail.smtp.auth", "true");/* Autorização */
+		properties.put("mail.smtp.starttls", "true");/* Autenticação */
+		properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor Gmail Google */
+		properties.put("mail.smtp.port", "465");/* Porta Do Servidor */
+		properties.put("mail.smtp.socketFactory.port", "465");/* Especifica a porta a ser utilizada pelo socket */
+		properties.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");/* Class Socket De Conexão SMTP */
+
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+			@Override
+			protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+				// TODO Auto-generated method stub
+				return new javax.mail.PasswordAuthentication(UserName, PassWord);
+			}
+
+		});
+
+		Address[] toUser = InternetAddress.parse(ListaDestinatario);
+
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(UserName, NomeRemetente));/* QUEM ESTA ENVIANDO */
+		message.setRecipients(Message.RecipientType.TO, toUser);/* PARA QUEM ESTA ENVIANDO */
+		message.setSubject(AssuntoEmail);/* ASSUNTO DO EMAIL */
+
+		/* PARTE 1 DO EMAIL QUE É O TEXTO EA DESCRIÇÃO DO EAMIL */
+
+		MimeBodyPart CorpoEmail = new MimeBodyPart();
+
+		if (EnvioHtmal) {
+			CorpoEmail.setContent(CorpoDoEmail, "text/html; charset=utf-8");
+		} else {
+			CorpoEmail.setText(CorpoDoEmail);/* CORPO DO EMAIL */
+		}
+
+		List<FileInputStream> ListaPdf = new ArrayList<FileInputStream>();/*LISTA DE PDF*/
+		ListaPdf.add(simuladordePdf());
+		ListaPdf.add(simuladordePdf());
+		ListaPdf.add(simuladordePdf());
+		ListaPdf.add(simuladordePdf());
+
+		Multipart multipart = new MimeMultipart();/* OBJETO QUE IRÁ JUNTAR AS PARTES DO EMAIL */
+		multipart.addBodyPart(CorpoEmail);/* ADICIONANDO CORPO DO EMAIL */
+
+		int anexo = 1;
+		
+		for (FileInputStream fileInputStream : ListaPdf) {/*PERCORRENDO A LISTA DE PDF*/
+
+			/* PARTE 2 DO EMAIL QUE OS ANEXOS */
+
+			MimeBodyPart AnexoDoEmail = new MimeBodyPart();/*INSTANCEANDO NOVO ANEXO*/
+			AnexoDoEmail.setDataHandler(new DataHandler(new ByteArrayDataSource(fileInputStream,
+					"application/pdf")));/* ADICIONANDO O CONTEUDO DO ANEXO E TIPO DO ARQUIVO */
+			AnexoDoEmail.setFileName("AnexoTest"+anexo+".pdf");
+
+			multipart.addBodyPart(AnexoDoEmail);/* ADICIONANDO ANEXO AO EMAIL */
+
+			anexo++;
+		}
+			message.setContent(multipart);/*ADICIONANTO TODO EMAIL CONSTRUIDO PARA ENVIO*/
+
+			Transport.send(message);/* ENVIANDO EMAIL */
+
+	}
 }
--- enviando-email/src/test/java/enviando/email/enviando_email/AppTest.java
+++ enviando-email/src/test/java/enviando/email/enviando_email/AppTest.java
@@ -1,5 +1,8 @@
 package enviando.email.enviando_email;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import org.junit.Test;
 
 /**
@@ -13,6 +16,8 @@ public class AppTest
 	@Test
     public void TesteMail() throws Exception{
     	
+		Instant inicio = Instant.now();
+		
 		StringBuilder stringBuilderTextoEmail = new StringBuilder(); 
 		stringBuilderTextoEmail.append("Ola,<br/><br/>");
 		stringBuilderTextoEmail.append("Você esta recebendo um email com html.<br/><br/>");
@@ -22,18 +27,24 @@ public class AppTest
 				+ " style=\"color: #000000; padding: 14px 25px; text-aligh: center; border-radius: 30px; text-decoration: none; display-inline: block; "
 				+ " font-family: courier; border: 3px solid green; font-weight: bolder;\"> ACESSAR</a> <br/><br/>");
 		
-    	ObjetoEnviaEmail objetoEnviaEmail = new ObjetoEnviaEmail("jailtoncapoeira2017@gmail.com, natanaelima2120@gmail.com", "Jailton Araujo", "Testanto Classe", stringBuilderTextoEmail.toString());
+    	ObjetoEnviaEmail objetoEnviaEmail = new ObjetoEnviaEmail("jailtoncapoeira2017@gmail.com", "Jailton Araujo", "Testanto Classe", stringBuilderTextoEmail.toString());
     	
     	//objetoEnviaEmail.EnviarEmail(true);
     	
-    	objetoEnviaEmail.EnviarEmailAnexo(true);
+    	objetoEnviaEmail.EnviarEmailListaDeAnexo(true);
     	
     	/*CASO EMAIL NÃO ESTEJA SENDO ENVIADO COLOQUE UM TEMPO DE ESPERA, MAS ISSO SO PODE SER FEITO EM TESTE*/
     	
     	Thread.sleep(5000);// PAUSA DE 5 SEGUNDOS*/
     	
+    	Instant Final = Instant.now();
+    	
+    	Duration duracao = Duration.between(inicio, Final);
+    	
+    	long dura = duracao.toSeconds();
     	
     	System.out.println("EMAIL ENVIADO COM SUCESSO");
+    	System.out.println("TEMPO TOTAL DE ENVIO DOS EAMILS: "+dura);
     	
     	
     	
